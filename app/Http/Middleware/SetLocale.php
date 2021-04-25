1d45a67f8af7e56aaf5b87bf7498ea2ef3c76ff9
rafacla <rafacla@github.com>
2021-04-25 17:24:37 -0300

    fixed: now localization sticks

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure                  $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->user = Auth::user();
+        app()->setLocale($this->user->language);
+        $locale = $request->route('locale') ?: Session::get('locale');
+
+        if ($locale) {
+            App::setLocale($locale);
+
+            /**
+             * We need to store the locale in the session because we don't want to pass it to each component we create.
+             * Also because we fetch the locale from the route but the url for the Livewire API endpoint only
+             * contains the component name.
+             */
+            Session::put('locale', $locale);
+        }
+
+        return $next($request);
+    }
+}
