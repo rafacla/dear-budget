b10d35861af6dd5cb9430b1ea30cb377fa6d4da9
rafacla <rafacla@github.com>
2021-07-06 21:05:25 -0300

    changed color of message on transaction and category views

--- source/resources/views/livewire/categories/list.blade.php
+++ source/resources/views/livewire/categories/list.blade.php
@@ -7,7 +7,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4 dark:bg-gray-600">
             @if (session()->has('message'))
-                <div class="bg-teal-100 -t-4 -teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
+                <div class="bg-green-500 border-t-4 border-green-600 rounded-b text-green-900 px-4 py-3 shadow-md my-3" role="alert">
                   <div class="flex">
                     <div>
                       <p class="text-sm">{{ session('message') }}</p>
